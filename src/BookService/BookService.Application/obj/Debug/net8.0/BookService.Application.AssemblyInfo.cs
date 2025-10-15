691ea42afbc56d13f633b50cc92510fc4b20cb21
nlhchinh <chinhnguyen1692000z@gmail.com>
2025-10-15 20:58:34 +0700

    Migration

--- src/BookService/BookService.Application/obj/Debug/net8.0/BookService.Application.AssemblyInfo.cs
+++ src/BookService/BookService.Application/obj/Debug/net8.0/BookService.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookService.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ccd8b473e89cbfa1c376a96ab71288bfabb2384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24fbfcf22cc9f3b69fd0501178f9f7ba0387ea54")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookService.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookService.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ src/BookService/BookService.Infrastructure/Migrations/20251015135759_AddImageUrlToBook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToBook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "BookTypes",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "BookTypes");
+        }
+    }
+}
